57ab547f0ab1e705a4afa48e9a913da4f79465d4
FS1612 <fsaverio9050@yahoo.it>
2022-10-11 16:38:11 +0200

    commenti

--- ProgettoRespa.net/ProgettoRespa.net/Form1.cs
+++ ProgettoRespa.net/ProgettoRespa.net/Form1.cs
@@ -12,20 +12,20 @@ namespace ProgettoRespa.net
 {
     public partial class Form1 : Form
     {
-        Boolean presente = false;
+        Boolean presente = false; // vale 1 se il giocatore è nella stanza, 0 altrimenti 
         int durataspostamento = 5000;
-        int spostamento = 297;
+        int spostamento = 297;// spostamento totale della porta ottenuto come differenza tra la posizione della porta completamente chiusa e quella della porta completamente chiusa
         int delta;
         int posiniziale = 250;
         int posAttuale;
-        Boolean Start = false;
-        Boolean Porta_timer = false;
-        Boolean Tempo_porta = false;
-        int chiusura = 0;
-        Boolean Chiusura_aggiornata = false; 
+        Boolean Start = false;// vale 1 se il giocatore ha premuto start, 0 altrimenti
+        Boolean Porta_timer = false;//vale 1 se il timer della porta è stato avviato, 0 altrimenti
+        Boolean Tempo_porta = false;// vale 1 se il tempo di apertura della porta è terminato, 0 altrimenti
+        int chiusura = 0;// conta quante volte si è chiusa la porta 
+        Boolean Chiusura_aggiornata = false; // vale 1 solo se viene aggiornato il valore di chiusura, 0 altrimenti
         int pos;
         double tempAttuale;
-        int tempIniziale = 19;
+        int tempIniziale = 19;//temperatura iniziale della stanza
         int temp1;
         public Form1()
         {
@@ -39,9 +39,9 @@ namespace ProgettoRespa.net
             Chiusura_aggiornata = false;
             
         }
-
         private void AggiornamentoPresenza()
         {
+            //funzione di supporto che verica la presenza del giocatore e aggiorna le variabili di stato 
             if (!presente)
             {
 
@@ -56,7 +56,6 @@ namespace ProgettoRespa.net
             }
             
         }
-
         private void button_START_Click(object sender, EventArgs e)
         {
             textStart.Text = "True";
@@ -66,9 +65,8 @@ namespace ProgettoRespa.net
 
             Start = true;
         }
-
         private void button_presenza_Click(object sender, EventArgs e)
-        {
+        {//funzione che gestisce la presenza del player tramite tasto
             if (presente)
             {
                 presente = false;
@@ -81,26 +79,22 @@ namespace ProgettoRespa.net
             }
 
         }
-
         private void startTimer_Tick(object sender, EventArgs e)
-        {
+        {//serve per riportare a false la variabile start appena il programma è avviato
             textStart.Text = "False";
             textStart.BackColor = Color.Red;
 
-        }
-
-       
+        }     
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
         private void button_RESET_Click(object sender, EventArgs e)
-        {
+        {//riporta il programma allo stato iniziale 
             textReset.Text = "True";
             textReset.BackColor = Color.Green;
             resetTimer.Enabled = true;
-           
+           // si può resettare il timer?
 
             //Start = false;
             Porta_timer = false;
@@ -111,9 +105,9 @@ namespace ProgettoRespa.net
             AggiornamentoPresenza();
             
         }
-
         private void masterTimer_Tick(object sender, EventArgs e)
-        {
+        {//ciclo di funzionamento del programma
+            
             pos = posAttuale + posiniziale;
             delta = masterTimer.Interval;
             if (Start)
@@ -122,7 +116,7 @@ namespace ProgettoRespa.net
                 Prossimitainterna_sensore.BackColor = Color.Red;
                 fcdSensore.BackColor = Color.Red;
                 textFcdPorta.BackColor = Color.Red;
-
+                // se ho avviato il programma(start=true), quando il player si trova all'esterno della porta (sensore prossimità esterno= true) la porta si deve aprire per permettere l'accesso del player per poi richiudersi trascorsi 5 secondi. Stesso meccanismo si avvia quando il player vuole uscire e dunque il sensore di prossimità esterno divaenta vero
                 if (TextSensProssimita_porta.Text.Equals("True") || (TextSensorePortaInterno.Text.Equals("True")))
                 {
                     posAttuale = posAttuale + (int)(delta * spostamento) / durataspostamento;
@@ -140,7 +134,7 @@ namespace ProgettoRespa.net
                     }
                 }
                 if (pos > 547)
-                {
+                {//se la porta è completamente aperta (posizione x=547) allora fcd è verificato e aumento iil contatore delle chiusure di uno dopo aver aggiornato il valore di Chiusura_aggiornata
                     if (!Chiusura_aggiornata)
                     {
                         Chiusura_aggiornata = true;
@@ -155,10 +149,10 @@ namespace ProgettoRespa.net
 
                 }
                 if (pos < 250)
-
+                    //se mi trovo a x=250 allora la porta è completamente chiusa e fcs è true
                 {
                     Chiusura_aggiornata = false;
-                  
+                  //la variabile booleana di appoccio chiusura porta viene settata a true appena la porta si apre e riportata a false appena la porta si richiude cosi che il contatore delle chiusure sia aggiornato una sola volta per ciclo
                     if (chiusura == 1)
                     {
                         presente = true;
@@ -177,7 +171,7 @@ namespace ProgettoRespa.net
 
                 }
                 if (TextSensProssimita_porta.Text.Equals("") && (TextSensorePortaInterno.Text.Equals("")))
-                {
+                {//la porta si chiude solo se i due sensori di prossiimita risultano falsi 
                     if (!Porta_timer)
                     {
                         Porta_timer = true;
@@ -195,21 +189,15 @@ namespace ProgettoRespa.net
 
 
                 porta.Left = pos;
-
+                
                 AggiornamentoTemperatura();
             }
         }
-
-        
-
         private void PortaTimer_Tick(object sender, EventArgs e)
-        {
+        {//timer che pone a true la variabile tempo_porta permettendo la ciusura della porta appena trascorso il tempo
             Tempo_porta = true;
             
         }
-
-        
-
         private void resetTimer_Tick(object sender, EventArgs e)
         {
             textReset.Text = "False";
@@ -217,15 +205,16 @@ namespace ProgettoRespa.net
         }
         private void AggiornamentoTemperatura() 
         {
-            
+            //funzione che deve gestire la temperatura della stanza per mantenerla tra il range compreso tra 19 gradi e la temperatura desiderata dall'utente 
             String temp = text_tempdesiderata.Text;
             
             if (temp != string.Empty)
 
             {
-                
+                // per poter paragonare le temperature entrambe devono essere intere, perciò deco convertire la temperatura inserita dall'utente nell'interfaccia da tipo String a tipo intero. Per la conversione uso il metodo int.Parse() che però può sollevare un'eccezione di tipo FormatException e, sollevata l'eccezione, per non far terminare il programma, la "gestisco" tramite il costrutto Try-catch
                 try
                 {
+                    // in questo blocco definisco ciò che il codice deve fare 
                     tempAttuale = int.Parse(textTemperatura.Text);
                      temp1 = int.Parse(temp);
                     timerTemp.Enabled = true;
@@ -235,27 +224,29 @@ namespace ProgettoRespa.net
                 }
                 catch (FormatException)
                 {
-                    
+                  //qualora volessi eseguire una porzione di codice dopo che viene sollevata l'eccezione   
                     
                 }
+                finally
+                {
+                    //per far eseguire al codice qualcosa indipendentemente dall'esito del try
+                }
                 
-            }
-            
-
-            
+            } 
         }
-
         private void timerTemp_Tick(object sender, EventArgs e)
         {
-             
+             // voglio che la temperatura aumenti solo se la temperatura della staza risulta minore della temperatura desiderata, se la porta è chiusa e se il personaggio è presente
             if (tempAttuale < temp1&&textFcsPorta.Text.Equals("True") && textPersonaggio.Text.Equals("Presente"))
             {
                 tempAttuale = tempAttuale + 0.5;
             }
+            //se la temperatura attuale della stanza è maggiore di quella iniziale allora, essendo la porta aperta, deve diminuire
             if(tempAttuale > tempIniziale && !textFcsPorta.Text.Equals("True"))
             {
                 tempAttuale = tempAttuale - 0.5;
             }
+            //la temperatura deve rimanere compresa fra la temperatura minima e la temperatura max
             if(tempAttuale > temp1)
             {
                 tempAttuale = temp1;
@@ -266,8 +257,6 @@ namespace ProgettoRespa.net
             }
 
             textTemperatura.Text = tempAttuale.ToString();
-        }
-
-        
+        } 
     }
 }
