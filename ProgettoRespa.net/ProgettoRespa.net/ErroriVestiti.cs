8350a85dc4fa05e1221a1f3f8944b3381e61e56b
Biuccia <fabiana.rossomando@gmail.com>
2022-10-25 18:15:12 +0200

    sensori corretti

--- ProgettoRespa.net/ProgettoRespa.net/ErroriVestiti.cs
+++ ProgettoRespa.net/ProgettoRespa.net/ErroriVestiti.cs
@@ -30,7 +30,7 @@ namespace ProgettoRespa.net
                 }
                 else
                 {
-                    if()
+                    //if()
                 }
             }
         }
